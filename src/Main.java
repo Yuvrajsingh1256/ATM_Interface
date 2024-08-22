e2b50f9091b2abaf394578baee0abe4e37fe4506
Yuvrajsingh1256 <yuvrajsingh6437@gmail.com>
2024-08-22 11:34:26 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+class BankAccount{
+    private double balance;
+    public BankAccount(double initialBalance){
+        this.balance= initialBalance;
+    }
+    public  double getBalance(){
+        return balance;
+    }
+    public void deposit(double amount){
+        if(amount >0){
+            balance += amount;
+            System.out.println("Deposit of $ "+amount+" successful. Current balance :$ " + balance);
+        }else{
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+    public void withdraw(double amount){
+        if(amount>0 && amount<= balance){
+            balance-= amount;
+            System.out.println("withdrawal of $ " + amount+ " successful.Current balance is : $ "+ balance);
+        }else{
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+}
+class ATM{
+    private BankAccount bankAccount;
+    public ATM(BankAccount bankAccount){
+        this.bankAccount=bankAccount;
+    }
+    public void displayMenu(){
+        System.out.println("ATN Menu :");
+        System.out.println("1. Check balance ");
+        System.out.println("2. deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. exit");
+    }
+    public void performTransaction(int choice, Scanner scanner){
+        switch (choice){
+            case 1:
+                System.out.println("Current balance : $" + bankAccount.getBalance());
+                break;
+            case 2:
+                System.out.println("Enter deposit amount : $");
+                double depositAmount = scanner.nextDouble();
+                bankAccount.deposit(depositAmount);
+                break;
+            case 3:
+                System.out.println("Enter withdraw amount: $");
+                double withdrawalAmount = scanner.nextDouble();
+                bankAccount.withdraw(withdrawalAmount);
+                break;
+            case 4:
+                System.out.println("Exiting ATM. Thank you!");
+                scanner.close();
+                System.exit(0);
+            default:
+                System.out.println("Invalid choice. Please select a valid option.");
+        }
+    }
+}
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter initial account balance: $");
+        double initialBalance = scanner.nextDouble();
+        BankAccount bankAccount = new BankAccount(initialBalance);
+        ATM atm = new ATM(bankAccount);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true){
+            atm.displayMenu();
+            System.out.println("select an option");
+            int choice = scanner.nextInt();
+            atm.performTransaction(choice,scanner);
         }
     }
 }
